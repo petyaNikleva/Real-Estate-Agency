b5e7a91824d62d5d5a05ac1933dc6dd270de57fb
P N <petyanikleva@yahoo.com>
2021-10-16 18:16:14 +0300

    add main and home pages

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 function expressConfig(app) {
-    app.use(express.static(path.resolve(__dirname, '../public')));
+    app.use('/static', express.static(path.resolve(__dirname, '../public')));
 }
 
 module.exports = expressConfig;
