74a63d1443647c2d020fc8069e60d293e3aac017
P N <petyanikleva@yahoo.com>
2021-10-16 19:03:41 +0300

    Add routes and homeController

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const app = express();
 const { PORT } = require('./constants');
+const app = express();
+
+const routes = require('./routes');
+
 
 require('./config/expressConfig')(app);
 require('./config/hbsConfig')(app);
 
-
-app.get('/', (req, res) => {
-    res.render('home');
-});
 
 app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}`))
