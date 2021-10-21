e37393f4383fbf66fe7c984dc628cd3e3080407f
P N <petyanikleva@yahoo.com>
2021-10-21 20:45:02 +0300

    Add housing for rent

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const authController = require('./controllers/authController');
+const housingController = require('./controllers/housingController');
+
 
 
 router.use(homeController);
 router.use('/auth', authController);
+router.use('/housing', housingController);
 
 
 module.exports = router; 
