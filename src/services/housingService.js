b061605f56641cedb29c8662739e975fee9ba67f
P N <petyanikleva@yahoo.com>
2021-11-02 14:05:13 +0200

    add imageUrl validation

--- src/services/housingService.js
+++ src/services/housingService.js
@@ -2,7 +2,6 @@ const Housing = require('../models/Housing');
 
 exports.getAll = () => Housing.find().lean();
 
-//exports.getOne = (housingId) => Housing.findById(housingId).lean();
 exports.getOne = (housingId) => Housing.findById(housingId).populate('tenants');
 
 exports.create = (housingData) => Housing.create(housingData);
@@ -10,12 +9,6 @@ exports.create = (housingData) => Housing.create(housingData);
 exports.getTopHouses = ()=> Housing.find().sort({createdAt: -1}).limit(3).lean();
 
 exports.addTenant = (housingId, tenantId) => {
-    // don't word for now ??
-    // ???Housing.findByIdAndUpdate(housingId, tenants: tenants-1)
-    // let housing = await housingService.getOne(req.params.housingId);
-    // housing.tenants.push(req.user._id);
-    // housing.availablePieces = housing.availablePieces - 1;
-    // return housing.save();
 
     return Housing.findOneAndUpdate(
         {_id: housingId}, 
@@ -30,7 +23,7 @@ exports.delete = (housingId) => Housing.findByIdAndDelete(housingId);
 
 exports.updateOne = (housingId, housingData) => Housing.findByIdAndUpdate(housingId, housingData);
 
-//exports.search = (text) => Housing.find({ type: text }).lean(); //if we want to be whole word ex. House
+//exports.search = (text) => Housing.find({ type: text }).lean(); //if we want to be whole word.
 exports.search = (text) => Housing.find({ type: {$regex: text, $options: 'i'}}).lean();  // - DONT WORK"???? if we search case insensitive and not full word
 
  
